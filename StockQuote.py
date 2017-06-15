25238345305a37243da70b948a58c400fc1b70a7
will-rice <wrice20@gmail.com>
2017-06-15 12:59:10 -0400

    Create StockQuote.py

--- StockQuote.py
+++ StockQuote.py
@@ -1,6 +1,6 @@
 import quandl
 
-quandl.ApiConfig.api_key = "6jkjXtsDx-zYybXseD-5"
+quandl.ApiConfig.api_key = "##############" <-Your key
 company = input("Enter Stock Symbol: ")
 
 closingQuote = quandl.get("WIKI/" + company, rows=1)
